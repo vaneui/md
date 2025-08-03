d5943183e3fbc776fb741e97dc358d1c59d6dcbf
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-08-03 20:04:17 +0300

    css cleanup

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -4,8 +4,6 @@ import './index.css'
 // Import VaneUI styles directly from node_modules
 import '@vaneui/ui/vars'
 import '@vaneui/ui/css'
-// Import only our MD styles
-import '../../src/styles/md.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
